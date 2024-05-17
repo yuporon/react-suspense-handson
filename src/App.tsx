64febb67ae3183fe56ba85f3cbaaed042827f27d
yuporon <ftb.y.m.kashima.07@gmail.com>
2024-05-17 18:08:20 +0900

    add: DataLoader

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { AlwaysSuspend } from "./components/AlwaysSuspend";
 import { SometimesSuspend } from "./components/SometimesSuspend";
 import { RenderingNotifier } from "./components/RenderingNotifier";
+import { DataLoader } from "./components/DataLoader";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,12 +13,13 @@ function App() {
       <h1 className="text-2xl">React App!</h1>
       <RenderingNotifier name="outside-Suspense" />
       <Suspense fallback={<div>Loading...</div>}>
-        {/* <AlwaysSuspend /> */}
+        {/* <AlwaysSuspend />
         <SometimesSuspend />
         <RenderingNotifier name="inside-Suspense" />
         <button className="border p-1" onClick={() => setCount((c) => c + 1)}>
           {count}
-        </button>
+        </button> */}
+        <DataLoader />
       </Suspense>
     </div>
   );
