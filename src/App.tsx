6cd65d6b9f99e3d2bf43a77cc10d324c44083644
yuporon <ftb.y.m.kashima.07@gmail.com>
2024-05-17 13:30:19 +0900

    add: AlwaysSuspend

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Suspense } from "react";
 import "./App.css";
+import { AlwaysSuspend } from "./components/AlwaysSuspend";
 
 function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <p>ここは表示されないよ</p>
+        <AlwaysSuspend />
+      </Suspense>
     </div>
   );
 }
