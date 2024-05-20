e4113ccc69e9f38887b5cf416bd221d34d219ea8
yuporon <ftb.y.m.kashima.07@gmail.com>
2024-05-20 17:39:59 +0900

    add: useData

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { AlwaysSuspend } from "./components/AlwaysSuspend";
 import { SometimesSuspend } from "./components/SometimesSuspend";
 import { RenderingNotifier } from "./components/RenderingNotifier";
-import { DataLoader } from "./components/DataLoader";
+import { DataLoader1, DataLoader2 } from "./components/DataLoader";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,13 +13,8 @@ function App() {
       <h1 className="text-2xl">React App!</h1>
       <RenderingNotifier name="outside-Suspense" />
       <Suspense fallback={<div>Loading...</div>}>
-        {/* <AlwaysSuspend />
-        <SometimesSuspend />
-        <RenderingNotifier name="inside-Suspense" />
-        <button className="border p-1" onClick={() => setCount((c) => c + 1)}>
-          {count}
-        </button> */}
-        <DataLoader />
+        <DataLoader1 />
+        <DataLoader2 />
       </Suspense>
     </div>
   );
